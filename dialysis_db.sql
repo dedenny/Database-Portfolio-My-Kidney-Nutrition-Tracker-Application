697d9c388e7e065bda68aa4b2f3c29e57e14a9a9
felzelle <calc@oregonstate.edu>
2022-04-23 22:01:59 -0400

    adding basic file for dialysis_db, altered kidney_disease.mwb, unsure of .bak file

---
+++ dialysis_db.sql
@@ -0,0 +1,148 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Patients`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Patients` (
+  `patient_id` INT NOT NULL AUTO_INCREMENT,
+  `last_name` VARCHAR(128) NOT NULL,
+  `first_name` VARCHAR(128) NOT NULL,
+  `age` INT NOT NULL,
+  `gender` VARCHAR(20) NULL,
+  `height` INT NOT NULL,
+  `weight` INT NOT NULL,
+  PRIMARY KEY (`patient_id`),
+  UNIQUE INDEX `idPatients_UNIQUE` (`patient_id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Foods`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Foods` (
+  `food_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  `phosphorous_content` INT NULL,
+  `phosphorous_units` VARCHAR(32) NOT NULL,
+  `sodium_content` INT NULL,
+  `sodium_unit` VARCHAR(32) NOT NULL,
+  `calories` INT NULL,
+  `calories_unit` VARCHAR(32) NOT NULL,
+  `potassium_content` INT NULL,
+  `potassium_unit` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`food_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Lab_Results`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Lab_Results` (
+  `lab_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  `phosphorus_lab` FLOAT NULL,
+  `phosphorus_lab_unit` VARCHAR(32) NOT NULL,
+  `potassium_lab` FLOAT NULL,
+  `potassium_lab_unit` VARCHAR(32) NOT NULL,
+  `sodium_lab` INT NULL,
+  `sodium_lab_unit` VARCHAR(32) NOT NULL,
+  `dialysis_adequacy_lab` FLOAT NULL,
+  `lab_results_time` DATETIME NULL,
+  `Patients_patient_id` INT NOT NULL,
+  PRIMARY KEY (`lab_id`, `Patients_patient_id`),
+  UNIQUE INDEX `lab_id_UNIQUE` (`lab_id` ASC) VISIBLE,
+  INDEX `fk_Lab_Results_Patients1_idx` (`Patients_patient_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Lab_Results_Patients1`
+    FOREIGN KEY (`Patients_patient_id`)
+    REFERENCES `mydb`.`Patients` (`patient_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Dialysis_Forms`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Dialysis_Forms` (
+  `dialysis_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  `location_type` VARCHAR(128) NOT NULL,
+  `adequacy_standard` FLOAT NOT NULL,
+  `Lab_Results_lab_id` INT NULL,
+  PRIMARY KEY (`dialysis_id`, `Lab_Results_lab_id`),
+  INDEX `fk_Dialysis_Forms_Lab_Results1_idx` (`Lab_Results_lab_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Dialysis_Forms_Lab_Results1`
+    FOREIGN KEY (`Lab_Results_lab_id`)
+    REFERENCES `mydb`.`Lab_Results` (`lab_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE Dialysis_Forms (
+  dialysis_id INT NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  location_type VARCHAR(128) NOT NULL,
+  adequacy_standard FLOAT NOT NULL,
+  PRIMARY KEY (dialysis_id);
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Patients_Food`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Patients_Food` (
+  `Foods_food_id` INT UNSIGNED NOT NULL,
+  `Patients_patient_id` INT NOT NULL,
+  `patient_food_time` DATETIME NOT NULL,
+  PRIMARY KEY (`Foods_food_id`, `Patients_patient_id`),
+  INDEX `fk_Food_has_Patients_Patients1_idx` (`Patients_patient_id` ASC) VISIBLE,
+  INDEX `fk_Food_has_Patients_Food_idx` (`Foods_food_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Food_has_Patients_Food`
+    FOREIGN KEY (`Foods_food_id`)
+    REFERENCES `mydb`.`Foods` (`food_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Food_has_Patients_Patients1`
+    FOREIGN KEY (`Patients_patient_id`)
+    REFERENCES `mydb`.`Patients` (`patient_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+insert into patients (patient_id, last_name, first_name, age,gender,height,weight) values 
+('100','Smith','Arlene','55','F','64','145'),
+('101','Rogers','Christopher','63','M','72','180'),
+('102','Harrison','Kayla','68','F','65','125'),
+('103','Jackson','Henry','74','M','75','200'),
+('104','Wonders','Brenda','91','F','60','92');
+
+INSERT INTO  lab_results (lab_id,phosphorus_lab,phosphorus_lab_unit,potassium_lab,potassium_lab_unit,
+sodium_lab,sodium_lab_unit,dialysis_adequacy_lab,lab_results_time,Patients_patient_id) VALUES
+('1','3.5','mg/dL','3.4','mEq/L','135','mEq/L','1.2','2022-05-07 23:22:05','3'),
+('2','5.5','mg/dL','3','mEq/L','142','mEq/L','1.7','2022-05-08 18:36:10','2'),
+('3','6.5','mg/dL','2.8','mEq/L','146','mEq/L','1.1','2022-05-01 20:20:06','4'),
+('4','10.5','mg/dL','6.6','mEq/L','144','mEq/L','0.6','2022-05-07 18:01:55','5'),
+('5','7.2','mg/dL','4.5','mEq/L','134','mEq/L','2.2','2022-05-11 10:19:25','1');
+
+INSERT INTO dialysis_forms (dialysis_id,name,location_type,adequacy_standard) VALUES
+('1','hemodialysis FMC','incenter','1.2'),
+('2','peritoneal Baxter','home','1.7');
